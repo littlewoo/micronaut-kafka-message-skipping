3de48b609f8ac596a075f556afba45bde49cda5d
Jakob Horndahl <jakob.horndahl@mojang.com>
2021-06-10 12:09:22 +0200

    Remove logging

--- src/test/java/com/github/littlewoo/MicronautKafkaThreadingTest.java
+++ src/test/java/com/github/littlewoo/MicronautKafkaThreadingTest.java
@@ -4,16 +4,12 @@ import com.github.littlewoo.kafka.MyProducer;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import javax.inject.Inject;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @MicronautTest
 class MicronautKafkaThreadingTest {
 
     @Inject
     MyProducer producer;
-
-    private final static Logger log = LoggerFactory.getLogger(MicronautKafkaThreadingTest.class);
 
     @Test
     void test() throws InterruptedException {
